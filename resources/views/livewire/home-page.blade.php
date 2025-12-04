f250066a71cdf693bc07fcb963b65cd2a1138c7a
mohammad <mypc264fm@gmail.com>
2025-12-05 02:28:03 +0330

    fix title bug

--- resources/views/livewire/home-page.blade.php
+++ resources/views/livewire/home-page.blade.php
@@ -1,9 +1,9 @@
-<div x-data="{ current_tab: {{$current_level}} , max_tab : 5}" class="w-full h-[100vh] l lg:h-[75vh] xl:h-[65vh] 2xl:h-[74vh]  lg:w-[85%] xl:w-[80%] 2xl:w-[70%]  border-seccondary/19 dark:border-white/20 border-1 rounded-none lg:rounded-[3rem] flex" >
+<div x-data="{ current_tab: {{$current_level}} , max_tab : 9}" class="w-full h-[100vh] l lg:h-[75vh] xl:h-[65vh] 2xl:h-[74vh]  lg:w-[85%] xl:w-[80%] 2xl:w-[70%]  border-seccondary/19 dark:border-white/20 border-1 rounded-none lg:rounded-[3rem] flex" >
     <div class="w-4/10  rounded-none lg:rounded-4xl not-lg:hidden " style="background-image: url('{{ asset('assets/images/banner.png')}}');background-size: cover" >
     </div>
     <div class="grid grid-rows-[1fr_9fr_1fr] relative w-full lg:w-6/10 p-3" >
         <div class="flex items-center flex-col" >
-            <h1 class="h-fit text-xl font-black" >ثبت نام بزگترین اعتکاف دانش اموزی</h1>
+            <h1 class="h-fit text-xl font-black" >ثبت نام بزرگترین اعتکاف دانش اموزی</h1>
             <div class="flex gap-3">
                 <span class="text-bold " >مسجد سید اصفهان</span>
                 <div class="flex gap-1" >
